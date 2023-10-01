8505f8af8285a1f26578115d5ebf205ea9451d82
jimenezzz <j.jimenez21@uniandes.edu.co>
2023-10-01 12:11:50 -0500

    added crud functions

--- crud.py
+++ crud.py
@@ -1,32 +1,27 @@
 from sqlalchemy.orm import Session
-
 import models, schemas
 
-
+#user functions
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
-
 
 def get_user_by_email(db: Session, email: str):
     return db.query(models.User).filter(models.User.email == email).first()
 
-
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
 
-
 def create_user(db: Session, user: schemas.UserCreate):
     fake_hashed_password = user.password + "notreallyhashed"
-    db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
+    db_user = models.User(email=user.email, name=user.name, phoneNumber=user.phoneNumber, role=user.role, bornDate=user.bornDate, gender=user.gender, hashed_password=fake_hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
 
-
+#item functions
 def get_items(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Item).offset(skip).limit(limit).all()
-
 
 def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
     db_item = models.Item(**item.dict(), owner_id=user_id)
@@ -34,3 +29,78 @@ def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
     db.commit()
     db.refresh(db_item)
     return db_item
+
+#image functions
+def get_images(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Image).offset(skip).limit(limit).all()
+
+def get_user_image(db: Session, user_id: int):
+    return db.query(models.Image).filter(models.Image.owner_id == user_id).first()
+
+def create_user_image(db: Session, image: schemas.ImageCreate, user_id: int):
+    db_image = models.Image(**image.dict(), owner_id=user_id)
+    db.add(db_image)
+    db.commit()
+    db.refresh(db_image)
+    return db_image
+
+#notification functions
+def get_notifications(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Notification).offset(skip).limit(limit).all()
+
+def get_user_notifications(db: Session, user_id: int):
+    return db.query(models.Notification).filter(models.Notification.owner_id == user_id).all()
+
+def create_user_notification(db: Session, notification: schemas.NotificationCreate, user_id: int):
+    db_notification = models.Notification(**notification.dict(), owner_id=user_id)
+    db.add(db_notification)
+    db.commit()
+    db.refresh(db_notification)
+    return db_notification
+
+#match functions
+def get_matches(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Match).offset(skip).limit(limit).all()
+
+def create_user_match(db: Session, match: schemas.MatchCreate, user_id: int):
+    db_match = models.Match(**match.dict(), user1_id=user_id)
+    db.add(db_match)
+    db.commit()
+    db.refresh(db_match)
+    return db_match
+
+def add_rate_to_match(db: Session, match_id: int, rate: str):
+    db_match = db.query(models.Match).filter(models.Match.id == match_id).first()
+    db_match.rate = rate
+    db.commit()
+    db.refresh(db_match)
+    return db_match
+
+def add_user2_to_match(db: Session, match_id: int, user_id: int):
+    db_match = db.query(models.Match).filter(models.Match.id == match_id).first()
+    db_match.user2_id = user_id
+    db.commit()
+    db.refresh(db_match)
+    return db_match
+
+#sport functions
+def get_sports(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Sport).offset(skip).limit(limit).all()
+
+def create_sport(db: Session, sport: schemas.SportCreate):
+    db_sport = models.Sport(**sport.dict())
+    db.add(db_sport)
+    db.commit()
+    db.refresh(db_sport)
+    return db_sport
+
+#level functions
+def get_levels(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Level).offset(skip).limit(limit).all()
+
+def create_level(db: Session, level: schemas.LevelCreate):
+    db_level = models.Level(**level.dict())
+    db.add(db_level)
+    db.commit()
+    db.refresh(db_level)
+    return db_level
